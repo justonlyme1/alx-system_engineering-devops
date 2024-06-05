6c9c814759776f127ea40e6935d311f9bca09e7b
justonlyme1 <taiwomiracleveecthor@gmail.com>
2024-06-05 08:50:41 -0700

    change task mod

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """
-module for number of subscribers for subreddit
+Contains the number_of_subscribers function
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    number of subscribers for subreddit
-    """
-    results = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers={'User-agent': 'my bot 0.1'})
-
-    if results.status_code == 200:
-        response_data = results.json()
-        subscribers = response_data['data']['subscribers']
-        return subscribers
-    else:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
