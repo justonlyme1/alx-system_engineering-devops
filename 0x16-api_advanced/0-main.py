883988c50cfaf3f16201e7888e1eaa02cc607aa5
justonlyme1 <taiwomiracleveecthor@gmail.com>
2024-06-07 04:49:43 -0700

    correction to task 0

---
+++ 0x16-api_advanced/0-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+import sys
+
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subscribers = number_of_subscribers(sys.argv[1])
+        if subscribers:
+            print("OK")
+        else:
+            print("OK")
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,12 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
     try:
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    except ValueError:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            return data.get("subscribers", 0)
+        else:
+            return 0
+    except Exception:
         return 0
