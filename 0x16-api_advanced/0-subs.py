c2f773175fe7d9d8e79725a6b68264e68a1a2838
justonlyme1 <taiwomiracleveecthor@gmail.com>
2024-06-07 00:43:01 -0700

    Task 0 redo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
-"""
+"""Contains number_of_subscribers function"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/VEECTHOR)"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    try:
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    except ValueError:
+        return 0
