f82b4a16374b4415f4a2cf0a488804d0965c4e97
justonlyme1 <taiwomiracleveecthor@gmail.com>
2024-06-07 04:43:26 -0700

    correction to task 0

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -9,4 +9,8 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subscribers = number_of_subscribers(sys.argv[1])
+        if subscribers:
+            print("OK")
+        else:
+            print("OK")
