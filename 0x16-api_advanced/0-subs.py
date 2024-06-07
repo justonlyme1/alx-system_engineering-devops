b155a5c90bf3b512c27a0f3ef2c790083fc7d291
justonlyme1 <taiwomiracleveecthor@gmail.com>
2024-06-07 09:08:12 -0700

    Task 0 correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Contains number_of_subscribers function"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return 0
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
     try:
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    except ValueError:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception:
         return 0
