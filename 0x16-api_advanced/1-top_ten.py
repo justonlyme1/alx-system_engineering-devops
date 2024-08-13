55203b7eeffc284da98e8f4f47168ae021be89e0
justonlyme1 <taiwomiracleveecthor@gmail.com>
2024-08-13 14:01:21 -0700

    task 1 retry-0

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+"""Contains top_ten function"""
+
 import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
@@ -12,8 +15,9 @@ def top_ten(subreddit):
 
     try:
         response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        # Check if the response is in JSON format
-        if response.headers.get('Content-Type') == 'application/json; charset=UTF-8':
+
+        # Check if the response status is OK and content type is JSON
+        if response.status_code == 200 and response.headers.get('Content-Type') == 'application/json; charset=UTF-8':
             try:
                 data = response.json()
                 results = data.get("data", {})
@@ -29,8 +33,7 @@ def top_ten(subreddit):
                 print("None")
         else:
             print("None")
-
+ 
     except requests.RequestException:
         # Handle network-related errors
         print("None")
-
